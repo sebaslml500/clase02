068e8dadcbef66dd054a729a8f8cb85340422ea4
Sebas <elsebaslml@gmail.com>
2024-06-23 17:01:19 -0500

    Actualizacion

---
+++ src/main/java/com/mycompany/tarea1/Estudiante.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tarea1;
+
+/**
+ *
+ * @author Sebas 
+ */
+public class Estudiante {
+    
+    public int codigo;
+    public double puntaje;
+  
+    public Estudiante(int codigo, double puntaje){
+        this.codigo=codigo;
+        this.puntaje=puntaje;
+    }       
+}
+    
+   
+   
+
